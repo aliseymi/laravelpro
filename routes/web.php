86801c51f245739a07f47b5a71ba8ece8911d9f0
seymi <seyedali1377mir@gmail.com>
2021-10-07 12:08:40 +0330

    using markdown mail and publish vendor/mail

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+//    $user = \App\Models\User::find(4);
+//    $user->notify(new \App\Notifications\LoginToWebsiteNotification());
     return view('welcome');
 });
 
